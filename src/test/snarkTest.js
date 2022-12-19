c9201dcf1a6fb3ccdce28857886988945498c315
kim <kyoungbin1996@gmail.com>
2022-12-19 16:31:08 +0900

    update front src

--- src/test/snarkTest.js
+++
@@ -1,2 +0,0 @@
-import {SnarkLibUtils, SnarkLib} from '../js-libsnark-opt/src/libsnark.interface.js'
-import _ from 'lodash';
--- src/utils/config.js
+++
@@ -1,9 +0,0 @@
-/* global BigInt */
-
-var Config = {
-    EC_TYPE : 'EC_ALT_BN128',
-    dataBlockNum : '530',
-    dataMaxBlockNum : '530',
-}
-
-export default Config;
