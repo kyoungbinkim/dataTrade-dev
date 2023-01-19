9afa06de370b31edf26b572dba4a9a2b6f44fda8
kim <kyoungbin1996@gmail.com>
2023-01-19 17:27:59 +0900

    add call isRegistered

--- server/src/controller/contractController.js
+++ server/src/controller/contractController.js
@@ -3,14 +3,20 @@ import {
     ContractIns,
     ContractJson,
     hexToDec
- } from '../core/contracts/utils';
+} from '../core/contracts/utils';
+
+import { 
+    isRegisteredData 
+} from '../core/contracts/registdata';
 
 export const getContractAbiController = (req, res) => {
     res.send(ContractJson.abi);
 }
 
-export const callIsRegisteredDataController = (req, res) => {
+export const callIsRegisteredDataController = async (req, res) => {
     const h_ct = req.param.h_ct;
 
-    
+    const flag = await isRegisteredData(h_ct);
+
+    res.send(flag);
 }
